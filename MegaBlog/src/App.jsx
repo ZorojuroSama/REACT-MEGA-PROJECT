4347a754cdad68f88bd39c219b42fb523a3871e8
Shubh <sahilraj2310@gmail.com>
2024-11-16 19:30:06 +0530

    dispatch

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,7 +1,22 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import "./App.css";
-
+import AuthService from "./appwrite/auth";
+import { login, logout } from "./redux store/authSlice";
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    AuthService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
 
   return (
     <>
