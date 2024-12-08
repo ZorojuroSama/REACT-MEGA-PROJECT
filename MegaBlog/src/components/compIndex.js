35ee26c14c00bba0c835245475f56b7f6e24d2e5
Shubh <sahilraj2310@gmail.com>
2024-12-08 20:07:21 +0530

    updated component index page

--- MegaBlog/src/components/compIndex.js
+++ MegaBlog/src/components/compIndex.js
@@ -1,14 +1,25 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container from "./container/Container"
-import Logo from "./Logo"
-import LogoutBtn from "./Header/LogoutBtn"
-import RTE from "./RTE"
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import SignUp from "./SignUp";
+import Login from "./Login";
+import Button from './Button';
+import PostForm from "./PostForm";
+import PostCard from "./PostCard";
+
 export { 
-    Header,
-    Footer,
-    Logo,
-    LogoutBtn,
-    Container,
-    RTE
-};
+    Header, 
+    Footer, 
+    Logo, 
+    LogoutBtn, 
+    Container, 
+    RTE, 
+    SignUp, 
+    Login,
+    PostForm,
+    Button,
+    PostCard,
+ };
